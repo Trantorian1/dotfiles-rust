628b26cd418606254a654dab83f786c1232a4cd9
trantorian <114066155+Trantorian1@users.noreply.github.com>
2024-05-30 02:21:48 +0200

    feat(plugins): added some new plugins

--- lua/custom/plugins/spectre.lua
+++ lua/custom/plugins/spectre.lua
@@ -4,16 +4,5 @@ return {
 		dependencies = {
 			'nvim-tree/nvim-web-devicons'
 		},
-		config = function()
-			local spectre = require('spectre')
-			spectre.setup()
-
-			vim.keymap.set('n', '<leader>sr', spectre.toggle(), {
-				desc = "[S]earch and [R]eplace"
-			})
-			vim.keymap.set('v', '<leader>sw', spectre.open_visual(), {
-				desc = "[S]earch [W]ord"
-			})
-		end
 	}
 }
