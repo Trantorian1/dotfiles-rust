74ccce641ffea70453a06e8d21565ff596df84bf
trantorian <114066155+Trantorian1@users.noreply.github.com>
2024-05-30 02:19:18 +0200

    feat(plugins): removed some plugins that were not being used

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,31 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	require "lua.custom.plugins.multicursor",
+	require "lua.custom.plugins.autopairs",
+	require "lua.custom.plugins.barbecue",
+	require "lua.custom.plugins.crates",
+	require "lua.custom.plugins.circom",
+	require "lua.custom.plugins.cursorline",
+	require "lua.custom.plugins.diffview",
+	require "lua.custom.plugins.git-blame",
+	require "lua.custom.plugins.inlayhints",
+	require "lua.custom.plugins.lazygit",
+	require "lua.custom.plugins.lualine",
+	require "lua.custom.plugins.multicursor",
+	require "lua.custom.plugins.neogen",
+	require "lua.custom.plugins.neoscroll",
+	require "lua.custom.plugins.oil",
+	require "lua.custom.plugins.peek",
+	require "lua.custom.plugins.rainbow-delimiters",
+	require "lua.custom.plugins.rustaceanvim",
+	require "lua.custom.plugins.scrollbar",
+	require "lua.custom.plugins.smart-splits",
+	require "lua.custom.plugins.spectre",
+	require "lua.custom.plugins.statuscol",
+	require "lua.custom.plugins.toggleterm",
+	require "lua.custom.plugins.trouble",
+	require "lua.custom.plugins.ts-context",
+	require "lua.custom.plugins.workspace-diagnostics",
+}
