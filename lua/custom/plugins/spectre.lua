881c5c8d71b09ceceb57a38968d01cc41ea26625
trantorian <114066155+Trantorian1@users.noreply.github.com>
2024-07-04 09:04:57 +0200

    feat(spectre): added custom keybindings to spectre

--- lua/custom/plugins/spectre.lua
+++ lua/custom/plugins/spectre.lua
@@ -4,5 +4,20 @@ return {
 		dependencies = {
 			'nvim-tree/nvim-web-devicons'
 		},
+		config = function()
+			local spectre = require("spectre")
+
+			spectre.setup()
+
+			vim.keymap.set('n', '<leader>srg', function()
+				spectre.toggle()
+			end,
+			{ desc = '[S]earch and [R]eplace [G]lobally' })
+
+			vim.keymap.set('n', '<leader>srw', function()
+				spectre.open_visual({select_word=true})
+			end,
+			{ desc = '[S]earch and [R]eplace [W]ord' })
+		end
 	}
 }
