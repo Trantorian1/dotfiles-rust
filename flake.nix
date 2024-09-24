b21a734b1e1b16cb71a931b25a8d2175d10c9e72
trantorian <114066155+Trantorian1@users.noreply.github.com>
2024-09-24 09:26:25 +0200

    fix(fmt)

--- flake.nix
+++ flake.nix
@@ -7,75 +7,78 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-      in
-      {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          # Rust
-          (rust-bin.nightly.latest.default.override {
-            extensions = [ "rust-src" ];
-            targets = [ "wasm32-unknown-unknown" ];
-          })
-          rust-analyzer
-          rustup
-          mdbook
-          mdbook-mermaid
-          # python
-          python312
-          poetry
-          mypy
-          # go
-          go
-          # TODO: move this to individual per-project flakes
-          protobuf
-          clang
-          llvmPackages.libclang
-          pkg-config
-          openssl
-          stdenv
-          # neovim
-          neovim
-          neovide
-          lua
-          xclip
-          unzip
-          # kickstart
-          git
-          gnumake
-          unzip
-          ripgrep
-          # spectre
-          gnused
-          # git ui
-          lazygit
-          # javascript
-          nodejs
-          deno
-          # formatters
-          nodePackages.fixjson
-          alejandra
-          ruff
-          isort
-          taplo
-        ];
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Rust
+            (rust-bin.nightly.latest.default.override {
+              extensions = ["rust-src"];
+              targets = ["wasm32-unknown-unknown"];
+            })
+            rust-analyzer
+            rustup
+            mdbook
+            mdbook-mermaid
+            # python
+            python312
+            poetry
+            mypy
+            # go
+            go
+            # TODO: move this to individual per-project flakes
+            protobuf
+            clang
+            llvmPackages.libclang
+            pkg-config
+            openssl
+            stdenv
+            # neovim
+            neovim
+            neovide
+            lua
+            xclip
+            unzip
+            # kickstart
+            git
+            gnumake
+            unzip
+            ripgrep
+            # spectre
+            gnused
+            # git ui
+            lazygit
+            # javascript
+            nodejs
+            deno
+            # formatters
+            nodePackages.fixjson
+            alejandra
+            ruff
+            isort
+            taplo
+          ];
 
-        RUST_BACKTRACE = 1;
+          RUST_BACKTRACE = 1;
 
-        shellHook = ''
-          export LIBCLANG_PATH=${pkgs.llvmPackages.libclang.lib}/lib
-          export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib";
-          export XDG_CONFIG_HOME=$HOME/Documents/code/.dotfiles
-          export SHELL=$(which zsh)
-        '';
-      };
-    }
-  );
+          shellHook = ''
+            export LIBCLANG_PATH=${pkgs.llvmPackages.libclang.lib}/lib
+            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib"
+            export XDG_CONFIG_HOME=$HOME/Documents/code/.dotfiles
+            export SHELL=$(which zsh)
+          '';
+        };
+      }
+    );
 }
-
